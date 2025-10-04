bdc91553416b77a3c10ef5723ba0ae4619d4662e
Shivam Bhagatkar <shivambhagatkar34@gmail.com>
2025-10-05 00:35:16 +0530

    Enable email sending for activation

--- src/main/java/in/shivambhagatkar/moneymanager/service/ProfileService.java
+++ src/main/java/in/shivambhagatkar/moneymanager/service/ProfileService.java
@@ -40,7 +40,7 @@ public class ProfileService {
         String activationLink = activationURL+"/api/v1.0/activate?token=" + newProfile.getActivationToken();
         String subject = "Activate your Money Manager Account";
         String body = "Click on the following link to activate your account " + activationLink;
-//        emailService.sendEmail(newProfile.getEmail(),subject, body);
+        emailService.sendEmail(newProfile.getEmail(),subject, body);
         return toDTO(newProfile);
     }
 
